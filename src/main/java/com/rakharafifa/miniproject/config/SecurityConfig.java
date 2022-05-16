80f6f92d02b5908e6cc8d6296fe67b930b934ede
RakhaRafifArifin <rakhara104@gmail.com>
2022-05-16 20:08:52 +0700

    edit cicd

--- src/main/java/com/rakharafifa/miniproject/config/SecurityConfig.java
+++ src/main/java/com/rakharafifa/miniproject/config/SecurityConfig.java
@@ -47,6 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
+            .antMatchers("/auth/**").permitAll()
+            .antMatchers("/miniproject/**").permitAll()
             .antMatchers("/swagger-ui/**").permitAll()
             .antMatchers("/v2/api-docs/**").permitAll()
             .antMatchers("/actuator/**").permitAll()
