f37ad6111d1eb11532dc0d698eaf2dd0f58e936d
RakhaRafifArifin <rakhara104@gmail.com>
2022-05-16 20:12:43 +0700

    menambahkan get mapping

--- src/main/java/com/rakharafifa/miniproject/MiniprojectApplication.java
+++ src/main/java/com/rakharafifa/miniproject/MiniprojectApplication.java
@@ -2,11 +2,17 @@ package com.rakharafifa.miniproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MiniprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniprojectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public void welcome(){
+		System.out.println("Selamat datang di Mini Project Rakha Rafif Arifin");
 	}
 }
