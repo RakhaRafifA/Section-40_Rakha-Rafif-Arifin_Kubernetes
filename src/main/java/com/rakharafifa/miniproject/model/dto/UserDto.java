9a28dbaa8929c789109b8441297ada9efc2a0ba8
RakhaRafifArifin <rakhara104@gmail.com>
2022-05-16 20:37:00 +0700

    perbaikan entity user

--- src/main/java/com/rakharafifa/miniproject/model/dto/UserDto.java
+++ src/main/java/com/rakharafifa/miniproject/model/dto/UserDto.java
@@ -8,8 +8,4 @@ public class UserDto {
     private String name;
     private String username;
     private String password;
-    private Long address_id;
-    private Long cart_id;
-    private Long transaction_id;
-    private Long wallet_id;
 }
